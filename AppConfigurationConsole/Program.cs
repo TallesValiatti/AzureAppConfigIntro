e45c5e9e694e03c42562180faee4aab99be44aeb
talles Destefani de souza valiatti <tallesvaliatti@talless-MacBook-Pro.local>
2021-08-13 18:27:27 -0300

    first

---
+++ AppConfigurationConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace AppConfigurationConsole
+{
+    class Program
+    {
+        private static readonly string _connectionString = "<ConnectionString>";
+         static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                            .AddAzureAppConfiguration(_connectionString)
+                            .Build();
+
+            Console.WriteLine(config["my-key"]);
+        }
+    }
+}
